03be8dda150aa2316a47b690be3d36ae1e9ee9fe
RodrigoDutra <dutra.rgdgd@gmail.com>
2020-06-08 12:17:49 -0300

    Add tensorboard image support

--- example.py
+++ example.py
@@ -8,6 +8,85 @@ from tensorflow.keras import backend as K
 from tensorboard.plugins.hparams import api as hp
 from random import choice, shuffle
 from pprint import pprint
+from datetime import datetime
+import io
+import itertools
+
+from six.moves import range
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.metrics
+
+
+def plot_to_image(figure):
+    """Converts the matplotlib plot specified by 'figure' to a PNG image and
+    returns it. The supplied figure is closed and inaccessible after this call."""
+    # Save the plot to a PNG in memory.
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    # Closing the figure prevents it from being displayed directly inside
+    # the notebook.
+    plt.close(figure)
+    buf.seek(0)
+    # Convert PNG buffer to TF image
+    image = tf.image.decode_png(buf.getvalue(), channels=4)
+    # Add the batch dimension
+    image = tf.expand_dims(image, 0)
+    return image
+
+
+def plot_confusion_matrix(cm, class_names):
+    """
+    Returns a matplotlib figure containing the plotted confusion matrix.
+
+    Args:
+        cm (array, shape = [n, n]): a confusion matrix of integer classes
+        class_names (array, shape = [n]): String names of the integer classes
+    """
+    figure = plt.figure(figsize=(8, 8))
+    plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+    plt.title("Confusion matrix")
+    plt.colorbar()
+    tick_marks = np.arange(len(class_names))
+    plt.xticks(tick_marks, class_names, rotation=45)
+    plt.yticks(tick_marks, class_names)
+
+    # Normalize the confusion matrix.
+    cm = np.around(cm.astype("float") / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+
+    # Use white text if squares are dark; otherwise black.
+    threshold = cm.max() / 2.0
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        color = "white" if cm[i, j] > threshold else "black"
+        plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
+
+    plt.tight_layout()
+    plt.ylabel("True label")
+    plt.xlabel("Predicted label")
+    return figure
+
+
+class CMCallback(tf.keras.callbacks.Callback):
+    def __init__(self, fw, model, test_data, test_labels):
+        self.fw = fw
+        self.model = model
+        self.test_data = test_data
+        self.test_labels = np.argmax(test_labels, axis=1)
+
+    def on_epoch_end(self, epoch, logs=None):
+        # Use the model to predict the values from the validation dataset.
+        test_pred = self.model.predict_classes(self.test_data)
+        # test_pred = np.argmax(test_pred_raw, axis=1)
+
+        # Calculate the confusion matrix.
+        cm = sklearn.metrics.confusion_matrix(self.test_labels, test_pred)
+        # Log the confusion matrix as an image summary.
+        figure = plot_confusion_matrix(cm, class_names=[str(i) for i in range(1,11)])
+        cm_image = plot_to_image(figure)
+
+        # Log the confusion matrix as an image summary.
+        with self.fw.as_default():
+            tf.summary.image("Confusion Matrix", cm_image, step=epoch)
 
 
 def get_model(input_shape, num_classes=10, hu_1=32, hu_2=64, hu_3=128, dp=0.25):
@@ -76,7 +155,7 @@ def rand_params(n_iter, *params):
     return hp_list
 
 
-def fit_model(log_dir, n_randomize=5):
+def fit_model(log_dir, n_randomize=5, epochs=10, num_classes=10):
     """Fits model to the MNIST dataset
 
     This function calls a randomizes of hyperparameters and for each combination
@@ -87,9 +166,6 @@ def fit_model(log_dir, n_randomize=5):
         log_dir     : Logs master directory
         n_randomize : Number of randomizations to find the best HParams
     """
-    num_classes = 10
-    epochs = 12
-
     # input image dimensions
     img_rows, img_cols = 28, 28
 
@@ -149,6 +225,7 @@ def fit_model(log_dir, n_randomize=5):
         tf_board = tf.keras.callbacks.TensorBoard(
             log_dir=run_dir, histogram_freq=1, write_graph=True, update_freq="epoch"
         )
+
         model = get_model(
             input_shape,
             hu_1=hparams["hu_1"],
@@ -156,6 +233,10 @@ def fit_model(log_dir, n_randomize=5):
             hu_3=hparams["hu_3"],
             dp=hparams["dp"] / 100,
         )
+
+        # Confusion matrix callback
+        file_writer_cm = tf.summary.create_file_writer(run_dir + '/cm')
+        tf_cm_callback = CMCallback(file_writer_cm, model, x_test, y_test)
 
         model.compile(
             loss=tf.keras.losses.categorical_crossentropy,
@@ -167,10 +248,10 @@ def fit_model(log_dir, n_randomize=5):
             x_train,
             y_train,
             batch_size=hparams["batch"],
-            epochs=1,
+            epochs=epochs,
             verbose=1,
             validation_data=(x_test, y_test),
-            callbacks=[tf_board],
+            callbacks=[tf_board, tf_cm_callback],
         )
         score = model.evaluate(x_test, y_test, verbose=0)
         print("Test loss:", score[0])
@@ -181,18 +262,25 @@ def fit_model(log_dir, n_randomize=5):
             accuracy = score[1]
             tf.summary.scalar(METRIC_ACCURACY, accuracy, step=1)
 
+            # Record some images from the training set
+            images = np.reshape(x_train[0:10], (-1, 28, 28, 1))
+            tf.summary.image(
+                "25 training data examples", images, max_outputs=10, step=0
+            )
+
 
 def call_tensorboard(file):
-    query = input('Do you wish to run tensorboard (y/n) ? ')
+    query = input("Do you wish to run tensorboard (y/n) ? ")
     Fl = query[0].lower()
-    if query == '' or not Fl in ['y','n']:
-        print('Please answer with yes or no!')
+    if query == "" or not Fl in ["y", "n"]:
+        print("Please answer with yes or no!")
         exit()
-    if Fl == 'y':
-        os.system('tensorboard --logdir=' + file)
+    if Fl == "y":
+        os.system("tensorboard --logdir=" + file)
         exit()
     else:
         exit()
+
 
 def main():
     # If there is a GPU limit its memory growth
@@ -210,7 +298,7 @@ def main():
     if not os.path.exists(logs_path):
         os.makedirs(logs_path)
 
-    fit_model(logs_path, n_randomize=5)
+    fit_model(logs_path, n_randomize=1, epochs=5)
     call_tensorboard(logs_path)
 
 
